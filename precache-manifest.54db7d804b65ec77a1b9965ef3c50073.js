d50bb5c5fe5ff9c6ad26331010afe71b64f7567c
Devesh-Darji <deveshdarji@hotmail.com>
2020-09-09 19:47:33 +0530

    Updates

---
+++ precache-manifest.54db7d804b65ec77a1b9965ef3c50073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7c18d9b90f0b5b38d50be2b7bdb911",
+    "url": "/Spending-app/index.html"
+  },
+  {
+    "revision": "9cd70a07cae18479c5a2",
+    "url": "/Spending-app/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "23b303e4c1c23d1a3530",
+    "url": "/Spending-app/static/js/2.5631936f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spending-app/static/js/2.5631936f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd70a07cae18479c5a2",
+    "url": "/Spending-app/static/js/main.aff2c27c.chunk.js"
+  },
+  {
+    "revision": "d466c7761db80b956a70",
+    "url": "/Spending-app/static/js/runtime-main.4cf41853.js"
+  }
+]);
